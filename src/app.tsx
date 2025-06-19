d49b4a802a47ba3c8a28ed0e21ac4f72cfbb10e7
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-19 18:00:07 +0530

    Updated code files: app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,37 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Homepage from "./pages/Homepage";
+import OSDetailPage from "./pages/OSDetailPage";
+import ComparisonPage from "./pages/ComparisonPage";
+import TimelinePage from "./pages/TimelinePage";
+import ResourcesPage from "./pages/ResourcesPage";
+import NotFound from "./pages/NotFound"; // Assuming NotFound.tsx exists
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/os/:osName" element={<OSDetailPage />} />
+          <Route path="/comparison" element={<ComparisonPage />} />
+          <Route path="/timeline" element={<TimelinePage />} />
+          <Route path="/resources" element={<ResourcesPage />} />
+          
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} /> {/* Always Include This Line As It Is. */}
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
